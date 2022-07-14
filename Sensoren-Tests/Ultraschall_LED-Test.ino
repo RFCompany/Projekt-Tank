c68de5e10a6fcde807991ed192c0be819a7cf55c
RFCompany <92989712+RFCompany@users.noreply.github.com>
2022-07-14 23:28:52 +0200

    comments

--- Sensoren-Tests/Ultraschall_LED-Test.ino
+++ Sensoren-Tests/Ultraschall_LED-Test.ino
@@ -1,43 +1,42 @@
-int trigger=7;                              
-int echo=6;
-int LED=12;
-int entfernung=0;
-int dauer=0;
+int trigger = 7;    // port für trigger
+int echo = 6;       // port echo
+int LED = 12;       // port LED
+int entfernung = 0; // variable entfernung wert 0
+int dauer = 0;      // variable dauer wert 0
 
 void setup()
 {
-Serial.begin (9600);
-pinMode(trigger, OUTPUT);
-pinMode(echo, INPUT);
-pinMode(12, OUTPUT);
+    Serial.begin(9600);
+    pinMode(trigger, OUTPUT); // trigger output
+    pinMode(echo, INPUT);     // echo interrupt
+    pinMode(12, OUTPUT);      // LED output
 }
 
 void loop()
 {
-digitalWrite(trigger, LOW);
-delay(5);
-digitalWrite(trigger, HIGH);
-delay(10);
-digitalWrite(trigger, LOW);
-dauer = pulseIn(echo, HIGH);
-entfernung = (dauer/2) * 0.03432;
+    digitalWrite(trigger, LOW);
+    delay(5);
+    digitalWrite(trigger, HIGH);
+    delay(10);
+    digitalWrite(trigger, LOW);
+    dauer = pulseIn(echo, HIGH);        // entfernungsmessung durch zeitabstände
+    entfernung = (dauer / 2) * 0.03432; // dauer/2 (hin und rückweg)     *schallgeschwindigkeit in cm/s
 
+    if (entfernung >= 500 || entfernung <= 0)
+    {
+        Serial.println("Kein Messwert");
+    }
 
-if (entfernung >= 500 || entfernung <= 0)
-{
-Serial.println("Kein Messwert");
-}
+    else
+    {
+        Serial.print(entfernung);
+    }
 
-else
-{
-Serial.print(entfernung);
-}
-
-if (entfernung <= 40)
-{
-digitalWrite(LED, HIGH);
-delay(entfernung*3);
-digitalWrite(LED, LOW);
-delay(entfernung*3);
-}
+    if (entfernung <= 40)
+    {
+        digitalWrite(LED, HIGH);
+        delay(entfernung * 3);
+        digitalWrite(LED, LOW);
+        delay(entfernung * 3);
+    }
 }
